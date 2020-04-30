2007f16545d4948fcbabf4e1091278853d0d3761
Ezequiel Ventura <39060105+ezventura@users.noreply.github.com>
2020-04-30 00:01:02 -0300

    Add files via upload

---
+++ 01-Adicion/adicion.cpp
@@ -0,0 +1,23 @@
+// 01-adicion.cpp
+// Suma de dos numeros
+// EV
+// 20200426
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int a,b;	
+	cout << "Digite un numero entero"<< endl;
+	cin >> a;
+	cout << "Digite otro numero entero"<< endl;
+	cin >> b;
+	
+	cout << "La suma es: " << a + b;
+	
+	return 0; 
+	
+}
+
